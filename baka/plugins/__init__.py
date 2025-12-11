59a06fbdabff4ba1efab11e4be5da481d636e54c
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-11 18:13:39 +0530

    Update __init__.py

--- baka/plugins/__init__.py
+++ baka/plugins/__init__.py
@@ -1,1 +1,25 @@
 
+# baka/plugins/__init__.py
+
+__all__ = [
+    "start",
+    "economy",
+    "game",
+    "admin",
+    "broadcast",
+    "fun",
+    "events",
+    "welcome",
+    "ping",
+    "chatbot",
+    "riddle",
+    "social",
+    "ai_media",
+    "waifu",
+    "collection",
+    "shop",
+    "daily",
+    "leaderboard"  # <--- YE LINE ADD KARNA ZAROORI HAI
+]
+
+from baka.plugins import *
