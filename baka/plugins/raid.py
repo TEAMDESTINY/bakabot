fc27d8169533d728193feb276a5335e16009e4e0
⏤͟͞ 𝙎𝙋𝘼𝙍𝙎𝙃 𝘽𝘼𝙉𝙄𝙔𝘼 <sparshshivhare2007@gmail.com>
2025-12-16 20:52:32 +0530

    Delete baka/plugins/raid.py

--- baka/plugins/raid.py
+++
@@ -1,3 +0,0 @@
-async def territory_raid(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # (Pichle code wala raid logic yahan aayega)
-    # Isse log dusre groups ke members ko challenge karenge.
